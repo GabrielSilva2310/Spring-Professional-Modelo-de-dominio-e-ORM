a6f49ec968ecc91f4673eb51419e52944ed18036
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-04-23 18:14:06 -0300

    Created project, Configured database and created entity participant

---
+++ src/main/java/com/SpringDevSuperior/SpringProfessional2/SpringProfessional2Application.java
@@ -0,0 +1,13 @@
+package com.SpringDevSuperior.SpringProfessional2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfessional2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfessional2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringDevSuperior/SpringProfessional2/SpringProfessional2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringDevSuperior.SpringProfessional2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfessional2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
